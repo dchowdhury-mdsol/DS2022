e5d2e4dcee9828ab86dabb12a05479175be27b1e
xdaiyan <daiyanch22@gmail.com>
2021-09-15 21:21:08 -0400

    PythonMathBasics

---
+++ PythonBasics/Math operands.py
@@ -0,0 +1,71 @@
+# Addition [+]
+x = 1
+y = 2
+print(x + y)
+
+# Subtraction [-]
+x = 2
+y = 1
+print(x - y)
+
+# Multiplication [*]
+x = 2
+y = 2
+print(x * y)
+
+# Division [/]
+x = 6
+y = 3
+print(x / y)
+
+# Modulo [%]
+x = 7
+y = 2
+print(x % y)
+
+# Floor [//]
+x = 13
+y = 2
+print(x // y)
+
+# Equal [==]
+x = 3
+y = 3
+z = 4
+print(x == y)
+print(x == z)
+
+# Greater than [>]
+x = 3
+y = 4
+z = 2
+print(x > y)
+print(x > z)
+
+# Less than [<]
+x = 3
+y = 4
+z = 2
+print(x < y)
+print(x < z)
+
+# Less than or equal to [<=]
+x = 3
+y = 3
+z = 2
+print(x <= y)
+print(x <= z)
+
+# Greater than or equal to [>=]
+x = 3
+y = 3
+z = 2
+print(x >= y)
+print(x >= z)
+
+# Not equal [!=]
+x = 3
+y = 3
+z = 2
+print(x != y)
+print(x != z)
